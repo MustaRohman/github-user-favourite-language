41cb5fda9972b79c66bcf5b03251657d0bf0665b
Musta Rohman <mustarohman@gmail.com>
2021-08-15 22:36:02 +0100

    Refactored index content into HomeContent component, using styled components

---
+++ components/HomeContent/HomeContent.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Main = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
