f643745d5ad674815ff29956821dbc32def0f677
Musta Rohman <mustarohman@gmail.com>
2021-08-16 16:18:52 +0100

    Added Circle CI badge. Added usage of github api token

--- pages/api/user/repos/[user].ts
+++ pages/api/user/repos/[user].ts
@@ -4,7 +4,9 @@ export default async function handler(req, res) {
   const { user } = req.query;
 
   try {
-    const data = await new GitHubClient().getUserRepos(user);
+    const data = await new GitHubClient(
+      process.env.NEXT_PUBLIC_API_TOKEN
+    ).getUserRepos(user);
     res.status(200).json({ data });
   } catch (e) {
     console.log("ERRROR", e);
